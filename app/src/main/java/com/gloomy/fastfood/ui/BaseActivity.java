22d588f3bcb88b36fa30b52a42821860f9bfa4b6
Gloomy <glomknight@gmail.com>
2017-03-14 20:57:24 +0700

    First Commit

---
+++ app/src/main/java/com/gloomy/fastfood/ui/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.gloomy.fastfood.ui;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Copyright © 2017 Gloomy
+ * Created by HungTQB on 14-Mar-17.
+ */
+@SuppressLint("Registered")
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/gloomy/fastfood/ui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.gloomy.fastfood.ui;
+
+import android.annotation.SuppressLint;
+
+import com.gloomy.fastfood.R;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Copyright © 2017 Gloomy
+ * Created by HungTQB on 14-Mar-17.
+ */
+@SuppressLint("Registered")
+@EActivity(R.layout.activity_main)
+public class MainActivity extends BaseActivity {
+
+    @AfterViews
+    void afterViews() {
+
+    }
+}
