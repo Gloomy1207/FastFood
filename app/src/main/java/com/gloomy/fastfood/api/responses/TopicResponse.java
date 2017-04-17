62352b0bd458cf8c1d230a5d1f5fa33593bab0aa
HungTQB <glomknight@gmail.com>
2017-04-17 15:49:41 +0700

    feature/topic_hot

---
+++ app/src/main/java/com/gloomy/fastfood/api/responses/TopicResponse.java
@@ -0,0 +1,22 @@
+package com.gloomy.fastfood.api.responses;
+
+import com.gloomy.fastfood.models.Topic;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import lombok.Getter;
+
+/**
+ * Copyright © 2017 AsianTech inc.
+ * Created by HungTQB on 17/04/2017.
+ */
+@Getter
+public class TopicResponse {
+    @SerializedName("content")
+    private List<Topic> topics;
+    @SerializedName("last")
+    private boolean isLast;
+    @SerializedName("number")
+    private int currentPage;
+}
